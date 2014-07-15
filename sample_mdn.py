e056fe667fd12bb113f0d9da4e430658c513bc93
John Whitlock <John-Whitlock@ieee.org>
2014-07-15 15:54:22 -0500

    Add additional files

---
+++ sample_mdn.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import re
+import os.path
+import random
+import urllib
+import webbrowser
+
+re_url = re.compile(r'\s*"url":\s"([^"]*)"')
+count = 0
+
+print "Go through data-human.json, and open each in the browser"
+
+# Fetch the file from github
+filename = "data-human.json"
+if not os.path.exists(filename):
+    print "Downloading data-human.json"
+    url = (
+        "https://raw.githubusercontent.com/webplatform/compatibility-data"
+        "/master/data-human.json")
+    urllib.urlretrieve(url, filename)
+
+# Extract the URLs
+urls = []
+with open(filename, 'r') as f:
+    for raw_line in f:
+        line = raw_line.strip()
+        match = re_url.match(line)
+        if match:
+            url = match.group(1)
+            urls.append(url)
+
+# Open them
+count = 0
+while True:
+    print "%d: %s" % (count, url)
+    count += 1
+    url = random.choice(urls)
+    webbrowser.open(url + '#Specifications')
+    x = raw_input("Enter to continue, or q+Enter to quit: ")
+    if x == 'q':
+        break
+
+print "%d URLs visited" % count
