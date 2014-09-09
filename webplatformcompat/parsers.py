3e8b66d825f4b676ae6da97c6ffe25836a9dd78d
John Whitlock <John-Whitlock@ieee.org>
2014-09-09 10:06:19 -0500

    Use dashes in JSON API resource names
    See bugzilla bug 1063550

---
+++ webplatformcompat/parsers.py
@@ -0,0 +1,9 @@
+from rest_framework_json_api.parsers import JsonApiParser \
+    as BaseJsonApiParser
+from rest_framework_json_api.utils import slug
+
+
+class JsonApiParser(BaseJsonApiParser):
+    def model_to_resource_type(self, model):
+        assert model
+        return slug(model._meta.verbose_name_plural)
--- webplatformcompat/renderers.py
+++ webplatformcompat/renderers.py
@@ -1,7 +1,12 @@
 from rest_framework.utils.encoders import JSONEncoder
 from rest_framework_json_api.renderers import JsonApiRenderer \
     as BaseJsonApiRender
+from rest_framework_json_api.utils import slug
 
 
 class JsonApiRenderer(BaseJsonApiRender):
     encoder_class = JSONEncoder
+
+    def model_to_resource_type(self, model):
+        assert model
+        return slug(model._meta.verbose_name_plural)
