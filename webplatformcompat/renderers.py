170c046154e24e403d224a8d313e4213d9faf995
John Whitlock <John-Whitlock@ieee.org>
2014-11-12 14:01:50 -0600

    bug 1063154 - Update to latest drf-json-api
    Drop workarounds that are now in main repository

--- webplatformcompat/renderers.py
+++ webplatformcompat/renderers.py
@@ -17,7 +17,6 @@ class JsonApiRenderer(BaseJsonApiRender):
     encoder_class = JSONEncoder
     wrappers = ([
         'wrap_jsonapi_aware',
-        'wrap_paginated',
     ] + BaseJsonApiRender.wrappers)
     dict_class = OrderedDict
 
@@ -27,40 +26,6 @@ class JsonApiRenderer(BaseJsonApiRender):
         if not jsonapi or not direct:
             raise WrapperNotApplicable('No jsonapi in context')
         return data
-
-    def wrap_paginated(self, data, renderer_context):
-        pagination_keys = ['count', 'next', 'previous', 'results']
-        for key in pagination_keys:
-            if not (data and key in data):
-                raise WrapperNotApplicable('Not paginated results')
-
-        view = renderer_context.get("view", None)
-        model = self.model_from_obj(view)
-        resource_type = self.model_to_resource_type(model)
-
-        # Use default wrapper for results
-        wrapper = self.wrap_default(data['results'], renderer_context)
-
-        # Add pagination metadata
-        pagination = self.dict_class()
-        pagination['previous'] = data['previous']
-        pagination['next'] = data['next']
-        pagination['count'] = data['count']
-        wrapper.setdefault('meta', self.dict_class())
-        wrapper['meta'].setdefault('pagination', self.dict_class())
-        wrapper['meta']['pagination'].setdefault(
-            resource_type, self.dict_class()).update(pagination)
-        return wrapper
-
-    def wrap_error(
-            self, data, renderer_context, keys_are_fields, issue_is_title):
-        # Fix bug about wrong name for non_field_error
-        wrapper = super(JsonApiRenderer, self).wrap_error(
-            data, renderer_context, keys_are_fields, issue_is_title)
-        for error in wrapper['errors']:
-            if error.get("path") == '/__all__':
-                error["path"] = '/-'
-        return wrapper
 
     def model_to_resource_type(self, model):
         if model:
