03da1e30058b9cd1fc4d02950963a100f8287756
John Whitlock <John-Whitlock@ieee.org>
2015-02-23 09:06:57 -0600

    bug 1063165 - First pass at account styling
    - Add toolbar for logged in status
    - Add about page
    - Convert login/logout to jinja2, Sync style
    - Add profile placeholder
    - Update copyright for 2015

--- webplatformcompat/helpers.py
+++ webplatformcompat/helpers.py
@@ -3,9 +3,13 @@
 This file is loaded by jingo and must be named helper.py
 """
 
+from datetime import date
+
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.utils.html import escape
 
+from allauth.account.utils import user_display
+from allauth.socialaccount import providers
 from jinja2 import contextfunction, Markup
 from jingo import register
 
@@ -67,4 +71,52 @@ def trans_str(context, trans_obj):
     return pick_translation(context, trans_obj)[0]
 
 
+@register.function
+def current_year():
+    return date.today().year
+
+
+@register.function
+@contextfunction
+def providers_media_js(context):
+    """Get a list of socialaccount providers.
+
+    Jingo version of providers_media_js from
+    allauth/socialaccount/templatetags/socialaccount.py
+    """
+    request = context['request']
+    ret = '\n'.join(
+        [p.media_js(request) for p in providers.registry.get_list()])
+    return ret
+
+
+@register.function
+def provider_login_url(
+        request, provider_id, process, scope=None, auth_params=None,
+        next=None):
+    """Get the login URL for a socialaccount provider.
+
+    Jingo version of provider_login_url from
+    allauth/socialaccount/templatetags/socialaccount.py
+    """
+    provider = providers.registry.by_id(provider_id)
+    query = {'process': process}
+    if scope:
+        query['scope'] = scope
+    if auth_params:
+        query['auth_params'] = auth_params
+    if next:
+        query['next'] = next
+    else:
+        next = request.POST.get('next') or request.GET.get('next')
+        if next:
+            query['next'] = next
+        elif process == 'redirect':
+            query['next'] = request.get_full_path()
+
+    # get the login url and append query as url parameters
+    return provider.get_login_url(request, **query)
+
+
 register.function(static)
+register.function(user_display)
