09bab0f91e1dc0fb0ebaf4f2a7227c9d107d7bd9
John Whitlock <John-Whitlock@ieee.org>
2015-05-15 13:58:56 -0500

    bug 1154349 - Update tools/gather_import_issues.py
    * Rename tools/gather_import_errors.py
    * Use new 'issues' terminology and endpoints
    * During import, count up to 100% rather than down to 0%
    * Write CSV with new data fields

--- tools/gather_import_issues.py
+++ tools/gather_import_issues.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 
 from collections import Counter
+from json import dumps
 from HTMLParser import HTMLParser
 import csv
 import logging
@@ -13,11 +14,11 @@ import time
 
 from client import Client
 
-logger = logging.getLogger('tools.gather_import_errors')
+logger = logging.getLogger('tools.gather_import_issues')
 my_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.realpath(os.path.join(my_dir, '..', 'data'))
-BY_URL_CSV_FILENAME = os.path.join(data_dir, 'import_errors_by_url.csv')
-BY_ERRORS_CSV_FILENAME = os.path.join(data_dir, 'import_error_counts.csv')
+BY_URL_CSV_FILENAME = os.path.join(data_dir, 'import_issues_by_url.csv')
+BY_ERRORS_CSV_FILENAME = os.path.join(data_dir, 'import_issue_counts.csv')
 
 
 class GatherLinks(HTMLParser):
@@ -39,33 +40,33 @@ class GatherLinks(HTMLParser):
         return ["/importer/%d.json" % i for i in sorted(self.import_ids)]
 
 
-def gather_import_errors(client, by_url_csv, by_error_csv):
-    error_rows = download_errors(client)
-    error_counts = Counter()
+def gather_import_issues(client, by_url_csv, by_issue_csv):
+    issue_rows = download_issues(client)
+    issue_counts = Counter()
 
-    # Write by-url CSV and count errors
+    # Write by-url CSV and count issues
     by_url_writer = csv.writer(by_url_csv)
     by_url_writer.writerow(
-        ["MDN Slug", "Import URL", "Source Start", "Source End", "Error",
-         "Rule"])
-    for row in error_rows:
+        ["MDN Slug", "Import URL", "Issue Slug", "Source Start", "Source End",
+         "Params"])
+    for row in issue_rows:
         err = row[4]
-        error_counts[err] += 1
+        issue_counts[err] += 1
         by_url_writer.writerow(row)
 
-    # Write by_error CSV
-    by_error_writer = csv.writer(by_error_csv)
-    by_error_writer.writerow(["Count", "Error"])
-    for err, count in error_counts.most_common():
-        by_error_writer.writerow((count, err))
+    # Write by_issue CSV
+    by_issue_writer = csv.writer(by_issue_csv)
+    by_issue_writer.writerow(["Count", "Issue"])
+    for err, count in issue_counts.most_common():
+        by_issue_writer.writerow((count, err))
 
     return {
-        'total': len(error_rows),
-        'unique': len(error_counts),
+        'total': len(issue_rows),
+        'unique': len(issue_counts),
     }
 
 
-def download_errors(client):
+def download_issues(client):
     # Gather import links
     start = time.time()
     logger.info("Gathering import URLs from %s...", client.base_url)
@@ -84,8 +85,8 @@ def download_errors(client):
     end = time.time()
     logger.info("Found %d import URLs in %0.1fs", total_urls, end - start)
 
-    # Gather errors
-    error_rows = []
+    # Gather issues
+    issue_rows = []
     start = time.time()
     for url_count, raw_url in enumerate(import_urls):
         url = client.base_url + raw_url
@@ -97,34 +98,28 @@ def download_errors(client):
         mdn_prefix = 'https://developer.mozilla.org/en-US/docs/'
         assert mdn_uri.startswith(mdn_prefix)
         mdn_slug = mdn_uri.replace(mdn_prefix, '', 1)
-        errors = resp_json['meta']['scrape']['raw']['errors']
-        for err in errors:
-            if isinstance(err, list):
-                start_pos, end_pos, msg = err[:3]
-                if len(err) == 4:
-                    rule = err[3]
-                else:
-                    rule = ""
-            else:
-                msg = err
-                start_pos = end_pos = rule = ''
-            error_rows.append(
-                (mdn_slug, human_import_url, start_pos, end_pos,
-                 msg.encode('utf-8'), rule))
+        issues = resp_json['meta']['scrape']['issues']
+        for issue in issues:
+            assert isinstance(issue, list)
+            issue_slug, start_pos, end_pos, raw_params = issue
+            params = dumps(raw_params)
+            issue_rows.append(
+                (mdn_slug, human_import_url, issue_slug, start_pos, end_pos,
+                 params))
         end = time.time()
         if (end - start) > 15:
-            percent = 100.0 * (total_urls - url_count + 1.0) / total_urls
+            percent = 100.0 * (1 - (total_urls - url_count + 1.0) / total_urls)
             logger.info(
-                "Found %d errors in %d imports (%d%%)",
-                len(error_rows), url_count + 1, percent)
+                "Found %d issues in %d imports (%d%%)",
+                len(issue_rows), url_count + 1, percent)
             start = time.time()
 
-    return error_rows
+    return issue_rows
 
 
 if __name__ == '__main__':
     import argparse
-    description = 'Gather import errors into CSVs.'
+    description = 'Gather import issues into CSVs.'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument(
         '-a', '--api',
@@ -164,22 +159,22 @@ if __name__ == '__main__':
     api = args.api
     if api.endswith('/'):
         api = api[:-1]
-    logger.info("Loading importer errors from %s" % api)
+    logger.info("Loading importer issues from %s" % api)
 
     # Initialize client
     client = Client(api)
 
-    # Gather errors and write to CSVs
+    # Gather issues and write to CSVs
     counts = {}
     by_url_csv = None
-    by_error_csv = None
+    by_issue_csv = None
     try:
         by_url_csv = open(BY_URL_CSV_FILENAME, "wb")
-        by_error_csv = open(BY_ERRORS_CSV_FILENAME, "wb")
-        counts = gather_import_errors(client, by_url_csv, by_error_csv)
+        by_issue_csv = open(BY_ERRORS_CSV_FILENAME, "wb")
+        counts = gather_import_issues(client, by_url_csv, by_issue_csv)
     finally:
-        if by_error_csv:
-            by_error_csv.close()
+        if by_issue_csv:
+            by_issue_csv.close()
         if by_url_csv:
             by_url_csv.close()
 
@@ -187,8 +182,8 @@ if __name__ == '__main__':
         c_unique = counts.get('unique', 0)
         c_total = counts.get('total', 0)
         logger.info(
-            "Errors collected: %d unique (%d total)", c_unique, c_total)
-        logger.info("Error details in %s", BY_URL_CSV_FILENAME)
-        logger.info("Error counts in %s", BY_ERRORS_CSV_FILENAME)
+            "Issues collected: %d unique (%d total)", c_unique, c_total)
+        logger.info("Issue details in %s", BY_URL_CSV_FILENAME)
+        logger.info("Issue counts in %s", BY_ERRORS_CSV_FILENAME)
     else:
-        logger.info("No errors found.")
+        logger.info("No issues found.")
