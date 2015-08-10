88f083ebd0ef1929bda4a53d6002e01a5abb95b3
John Whitlock <jwhitlock@mozilla.com>
2015-08-10 17:08:48 -0500

    bug 1171957 - unicode literals
    Use unicode literals in mdn.data, so that new IDs will be recognized by
    mdn.utils.is_new_id.

--- mdn/data.py
+++ mdn/data.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 """Data lookup operations for MDN parsing."""
-
+from __future__ import unicode_literals
 from collections import namedtuple
 
 from webplatformcompat.models import (
