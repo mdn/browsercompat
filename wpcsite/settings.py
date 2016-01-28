aab2fa81eb995e83f1966ba11b0400913d124058
John Whitlock <jwhitlock@mozilla.com>
2016-01-28 14:34:55 -0600

    fix bug 1218237 - Use decouple to load settings
    Use python-decouple to load settings from the environment or a
    environment file.

--- wpcsite/settings.py
+++ wpcsite/settings.py
@@ -18,7 +18,7 @@ $ vi $VIRTUAL_ENV/bin/predeactivate
 unset DJANGO_DEBUG
 $ source $VIRTUAL_ENV/bin/postactivate
 
-A second way to to use autoenv and a .env file.  See env.dist.
+A second way to to use an .env file.  See env.dist.
 
 To set environment variables in heroku environment
 $ heroku config
@@ -62,45 +62,39 @@ SERVER_EMAIL - Email "From" address for error messages to admins
 STATIC_ROOT - Overrides STATIC_ROOT
 
 """
-
-# Build paths inside the project like this: rel_path('folder', 'file')
-from os import environ, path
+from os import path
 import sys
-import dj_database_url
+
+from decouple import config, Csv
+from dj_database_url import parse as db_url
 
 BASE_DIR = path.dirname(path.dirname(__file__))
 
 
 def rel_path(*subpaths):
+    """Convert a list of relative paths to an absolute path."""
     return path.join(BASE_DIR, *subpaths)
 
 
-def env_to_bool(environ_key, default):
-    """Convert an environment string to a bool."""
-    if environ_key in environ:
-        return environ[environ_key] in (1, '1')
-    else:
-        return default
-
+cast_list = Csv()  # Convert comma-separated strings to lists
 
 # Detect that we're running tests
 TESTING = sys.argv[1:2] == ['test']
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get(
-    'SECRET_KEY', 'jl^8zustdy8ht@=abml-j@%hp7hr-0u-41hb*1=duc91a%=9+%')
+SECRET_KEY = config(
+    'SECRET_KEY',
+    default='jl^8zustdy8ht@=abml-j@%hp7hr-0u-41hb*1=duc91a%=9+%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = environ.get('DJANGO_DEBUG', '0') in (1, '1')
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS', '').split(',')
-if environ.get('SECURE_PROXY_SSL_HEADER'):
-    raw = environ['SECURE_PROXY_SSL_HEADER']
-    SECURE_PROXY_SSL_HEADER = tuple(raw.split(','))
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=cast_list)
+SECURE_PROXY_SSL_HEADER = config(
+    'SECURE_PROXY_SSL_HEADER', default='', cast=cast_list) or None
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
@@ -133,8 +127,8 @@ INSTALLED_APPS = [
     'mdn',
     'webplatformcompat',
 ]
-if environ.get('EXTRA_INSTALLED_APPS'):
-    INSTALLED_APPS + environ['EXTRA_INSTALLED_APPS'].split(',')
+INSTALLED_APPS.extend(
+    config('EXTRA_INSTALLED_APPS', default='', cast=cast_list))
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -153,43 +147,37 @@ ROOT_URLCONF = 'wpcsite.urls'
 WSGI_APPLICATION = 'wpcsite.wsgi.application'
 
 # django-allauth requires some settings
-SITE_ID = int(environ.get('SITE_ID', '1'))
+SITE_ID = config('SITE_ID', default='1', cast=int)
 
 # Email settings
-if 'ADMIN_NAMES' in environ and 'ADMIN_EMAILS' in environ:
-    names = [x.strip() for x in environ['ADMIN_NAMES'].split(',')]
-    emails = [x.strip() for x in environ['ADMIN_EMAILS'].split(',')]
-    ADMINS = zip(names, emails)
-if 'EMAIL_BACKEND' in environ:
-    EMAIL_BACKEND = environ['EMAIL_BACKEND']
-if 'EMAIL_HOST' in environ:
-    EMAIL_HOST = environ['EMAIL_HOST']
-if 'EMAIL_PORT' in environ:
-    EMAIL_PORT = environ['EMAIL_PORT']
-if 'EMAIL_HOST_USER' in environ:
-    EMAIL_HOST_USER = environ['EMAIL_HOST_USER']
-if 'EMAIL_HOST_PASSWORD' in environ:
-    EMAIL_HOST_PASSWORD = environ['EMAIL_HOST_PASSWORD']
-if 'EMAIL_USE_TLS' in environ:
-    EMAIL_USE_TLS = (environ['EMAIL_USE_TLS'] in (1, '1'))
-if 'EMAIL_USE_SSL' in environ:
-    EMAIL_USE_SSL = (environ['EMAIL_USE_SSL'] in (1, '1'))
-if 'EMAIL_SUBJECT_PREFIX' in environ:
-    EMAIL_SUBJECT_PREFIX = environ['EMAIL_SUBJECT_PREFIX']
-if 'SERVER_EMAIL' in environ:
-    SERVER_EMAIL = environ['SERVER_EMAIL']
-if 'DEFAULT_FROM_EMAIL' in environ:
-    DEFAULT_FROM_EMAIL = environ['DEFAULT_FROM_EMAIL']
-if 'EMAIL_FILE_PATH' in environ:
-    EMAIL_FILE_PATH = environ['EMAIL_FILE_PATH']
+ADMINS = zip(
+    config('ADMIN_NAMES', default='', cast=cast_list),
+    config('ADMIN_EMAILS', default='', cast=cast_list))
+EMAIL_BACKEND = config(
+    'EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
+EMAIL_SUBJECT_PREFIX = config('EMAIL_SUBJECT_PREFIX', default='[Django] ')
+SERVER_EMAIL = config('SERVER_EMAIL', default='root@localhost')
+DEFAULT_FROM_EMAIL = config(
+    'DEFAULT_FROM_EMAIL', default='webmaster@localhost')
+_email_file_path = config('EMAIL_FILE_PATH', default='')
+if _email_file_path:
+    EMAIL_FILE_PATH = _email_file_path
 
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default':
-        dj_database_url.config(default='sqlite:///' + rel_path('db.sqlite3'))
+    'default': config(
+        'DATABASE_URL',
+        default='sqlite:///' + rel_path('db.sqlite3'),
+        cast=db_url)
 }
 
 # Internationalization
@@ -233,8 +221,7 @@ TEMPLATES = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-if environ.get('STATIC_ROOT'):
-    STATIC_ROOT = environ['STATIC_ROOT']
+STATIC_ROOT = config('STATIC_ROOT', default='') or None
 STATIC_URL = '/static/'
 
 #
@@ -244,17 +231,28 @@ STATIC_URL = '/static/'
 # https://devcenter.heroku.com/articles/django-memcache
 # Otherwise, use django-pylibmc environment variables or locmen
 # https://github.com/jbalogh/django-pylibmc
+# TODO: see if standard django.core.cache.backends.memcached.PyLibMCCache
+#  can be configured to work with MemCachier, or switch to Redis
 
-# In Heroku, load Memcachier settings into django-pylibmc
-if environ.get('MEMCACHIER_SERVERS'):
-    environ['MEMCACHE_SERVERS'] = environ.get(
-        'MEMCACHIER_SERVERS', '').replace(',', ';')
-if environ.get('MEMCACHIER_USERNAME'):
-    environ['MEMCACHE_USERNAME'] = environ.get('MEMCACHIER_USERNAME', '')
-if environ.get('MEMCACHIER_PASSWORD'):
-    environ['MEMCACHE_PASSWORD'] = environ.get('MEMCACHIER_PASSWORD', '')
+_memcachier_servers = config(
+    'MEMCACHIER_SERVERS', default='').replace(',', ';')
+_memcache_servers = config('MEMCACHE_SERVERS', default=_memcachier_servers)
+_redis_url = config('REDIS_URL', default='')
 
-if environ.get('MEMCACHE_SERVERS') and not TESTING:
+if _memcache_servers and not TESTING:
+    _memcachier_username = config('MEMCACHIER_USERNAME', default='')
+    _memcachier_password = config('MEMCACHIER_PASSWORD', default='')
+    _memcache_username = config(
+        'MEMCACHE_USERNAME', default=_memcachier_username)
+    _memcache_password = config(
+        'MEMCACHE_PASSWORD', default=_memcachier_password)
+
+    # Set environment variables because that's what django_pylibmc needs.
+    from os import environ
+    environ['MEMCACHE_SERVERS'] = _memcache_servers
+    environ['MEMCACHE_USERNAME'] = _memcache_username
+    environ['MEMCACHE_PASSWORD'] = _memcache_password
+
     # Use memcache
     CACHES = {
         'default': {
@@ -272,12 +270,12 @@ if environ.get('MEMCACHE_SERVERS') and not TESTING:
             },
         },
     }
-elif environ.get('REDIS_URL') and not TESTING:
+elif _redis_url and not TESTING:
     # Use redis
     CACHES = {
         'default': {
             'BACKEND': 'django_redis.cache.RedisCache',
-            'LOCATION': environ['REDIS_URL'],
+            'LOCATION': _redis_url,
             'OPTIONS': {
                 'CLIENT_CLASS': 'django_redis.client.DefaultClient'
             }
@@ -299,13 +297,14 @@ PAGINATE_VIEW_FEATURE = 50
 LOGIN_URL = '/accounts/login/'
 
 # MDN Scraping
-if environ.get('MDN_ALLOWED_URL_PREFIXES') and not TESTING:
-    raw = environ['MDN_ALLOWED_URL_PREFIXES']
-    MDN_ALLOWED_URLS = tuple(raw.split(','))
-else:
+if TESTING:
     MDN_ALLOWED_URLS = ('https://developer.mozilla.org/en-US/', )
-
-MDN_SHOW_REPARSE = env_to_bool('MDN_SHOW_REPARSE', DEBUG)
+else:
+    MDN_ALLOWED_URLS = config(
+        'MDN_ALLOWED_URL_PREFIXES',
+        default='https://developer.mozilla.org/en-US/',
+        cast=cast_list)
+MDN_SHOW_REPARSE = config('MDN_SHOW_REPARSE', default=DEBUG, cast=bool)
 
 #
 # 3rd Party Libraries
@@ -353,21 +352,23 @@ CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 if TESTING:
     CELERY_ALWAYS_EAGER = True
 else:
-    if environ.get('BROKER_URL'):
-        BROKER_URL = environ['BROKER_URL']
-        _eager_default = 0
+    _broker_url = config('BROKER_URL', default='')
+    if _broker_url:
+        BROKER_URL = _broker_url
+        _eager_default = False
     else:
-        _eager_default = 1
-    CELERY_ALWAYS_EAGER = (
-        environ.get('CELERY_ALWAYS_EAGER', _eager_default) not in (0, '0'))
-    if environ.get('CELERY_RESULT_BACKEND'):
-        CELERY_RESULT_BACKEND = environ['CELERY_RESULT_BACKEND']
+        _eager_default = True
+    CELERY_ALWAYS_EAGER = config(
+        'CELERY_ALWAYS_EAGER', default=_eager_default, cast=bool)
+    _celery_result_backend = config('CELERY_RESULT_BACKEND', default='')
+    if _celery_result_backend:
+        CELERY_RESULT_BACKEND = _celery_result_backend
 
 # DRF Cached Instances - avoid DB reads for Django REST Framework
-USE_DRF_INSTANCE_CACHE = (
-    environ.get('USE_DRF_INSTANCE_CACHE', '1') not in (0, '0'))
-DRF_INSTANCE_CACHE_POPULATE_COLD = (
-    environ.get('DRF_INSTANCE_CACHE_POPULATE_COLD', '1') not in (0, '0'))
+USE_DRF_INSTANCE_CACHE = config(
+    'USE_DRF_INSTANCE_CACHE', default=True, cast=bool)
+DRF_INSTANCE_CACHE_POPULATE_COLD = config(
+    'DRF_INSTANCE_CACHE_POPULATE_COLD', default=True, cast=bool)
 
 # CORS Middleware
 CORS_ORIGIN_ALLOW_ALL = True
@@ -376,11 +377,11 @@ CORS_ALLOW_CREDENTIALS = True
 # django-allauth
 SOCIALACCOUNT_PROVIDERS = {
     'fxa': {
-        'OAUTH_ENDPOINT': environ.get(
+        'OAUTH_ENDPOINT': config(
             'FXA_OAUTH_ENDPOINT',
-            'https://oauth.accounts.firefox.com/v1'),
-        'PROFILE_ENDPOINT': environ.get(
+            default='https://oauth.accounts.firefox.com/v1'),
+        'PROFILE_ENDPOINT': config(
             'FXA_PROFILE_ENDPOINT',
-            'https://profile.accounts.firefox.com/v1'),
+            default='https://profile.accounts.firefox.com/v1'),
     }
 }
