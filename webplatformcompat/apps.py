309ee047be41f42f0bf018d9cf6ae5277262fedb
John Whitlock <jwhitlock@mozilla.com>
2016-02-11 16:24:12 -0600

    bug 1247690 - Move signal registration to apps
    Instead of using @register decorator, manually register signal handlers
    in the AppConfig.ready function. This removes a code path where new user
    could go through the caching system before they get the default
    change-resource group added.

--- webplatformcompat/apps.py
+++ webplatformcompat/apps.py
@@ -1,13 +1,50 @@
 # -*- coding: utf-8 -*-
 """Application configuration."""
 from django.apps import AppConfig
+from django.db.models.signals import post_delete, post_save, m2m_changed
 
 
 class WebPlatformCompatConfig(AppConfig):
+    """Configuration for the webplatformcompat app."""
+
     name = 'webplatformcompat'
     verbose_name = 'WebPlatformCompat'
 
     def ready(self):
+        """Register signal handlers when models are loaded."""
         super(WebPlatformCompatConfig, self).ready()
-        import webplatformcompat.signals
-        assert webplatformcompat.signals, 'Failed to make flake8 happy'
+        from django.contrib.auth.models import User
+        from webplatformcompat.models import (
+            Browser, Feature, Maturity, Section, Specification,
+            Support, Version)
+        from webplatformcompat.signals import (
+            add_user_to_change_resource_group,
+            feature_sections_changed_update_order,
+            post_delete_update_cache,
+            post_save_update_cache)
+
+        # Add default API permissions to new users
+        post_save.connect(
+            add_user_to_change_resource_group,
+            sender=User,
+            dispatch_uid='add_user_to_change_resource_group')
+
+        # Invalidate instance cache when features-to-sections changes
+        m2m_changed.connect(
+            feature_sections_changed_update_order,
+            sender=Feature.sections.through,
+            dispatch_uid='m2m_changed_feature_section')
+
+        # Invalidate instance cache on model changes
+        for model in (
+                Browser, Feature, Maturity, Section, Specification,
+                Support, Version, User):
+            name = model.__name__
+            post_delete.connect(
+                post_delete_update_cache,
+                sender=model,
+                dispatch_uid='post_delete_update_cache_%s' % name)
+            post_save.connect(
+                post_save_update_cache,
+                sender=model,
+                dispatch_uid='post_save_update_cache_%s' % name)
--- webplatformcompat/signals.py
+++ webplatformcompat/signals.py
@@ -3,20 +3,9 @@
 
 from __future__ import unicode_literals
 
-from django.conf import settings
-from django.db.models.signals import post_delete, post_save, m2m_changed
-from django.dispatch import receiver
-
 from .models import Feature, Section
 
-cached_model_names = (
-    'Browser', 'Feature', 'Maturity', 'Section', 'Specification',
-    'Support', 'Version', 'User')
 
-
-@receiver(
-    m2m_changed, sender=Feature.sections.through,
-    dispatch_uid='m2m_changed_feature_section')
 def feature_sections_changed_update_order(
         sender, instance, action, reverse, model, pk_set, **kwargs):
     """Maintain feature.section_order."""
@@ -49,39 +38,30 @@ def feature_sections_changed_update_order(
         update_cache_for_instance('Section', section.pk, section)
 
 
-@receiver(post_delete, dispatch_uid='post_delete_update_cache')
 def post_delete_update_cache(sender, instance, **kwargs):
+    """Invalidate the cache when an instance is deleted."""
     name = sender.__name__
-    if name in cached_model_names:
-        delay_cache = getattr(instance, '_delay_cache', False)
-        if not delay_cache:
-            from .tasks import update_cache_for_instance
-            update_cache_for_instance(name, instance.pk, instance)
+    delay_cache = getattr(instance, '_delay_cache', False)
+    if not delay_cache:
+        from .tasks import update_cache_for_instance
+        update_cache_for_instance(name, instance.pk, instance)
 
 
-@receiver(post_save, dispatch_uid='post_save_update_cache')
 def post_save_update_cache(sender, instance, created, raw, **kwargs):
+    """Invalidate the cache when an instance is created or updated."""
     if raw:
         return
     name = sender.__name__
-    if name in cached_model_names:
-        delay_cache = getattr(instance, '_delay_cache', False)
-        if not delay_cache:
-            from .tasks import update_cache_for_instance
-            update_cache_for_instance(name, instance.pk, instance)
+    delay_cache = getattr(instance, '_delay_cache', False)
+    if not delay_cache:
+        from .tasks import update_cache_for_instance
+        update_cache_for_instance(name, instance.pk, instance)
 
 
-#
-# New user signals
-#
-@receiver(
-    post_save, sender=settings.AUTH_USER_MODEL,
-    dispatch_uid='add_user_to_change_resource_group')
 def add_user_to_change_resource_group(
         signal, sender, instance, created, raw, **kwargs):
+    """Add change-resource permission to new users."""
     if created and not raw:
         from django.contrib.auth.models import Group
         instance.groups.add(Group.objects.get(name='change-resource'))
-        if hasattr(instance, 'group_names'):
-            del instance.group_names
         post_save_update_cache(sender, instance, created, raw, **kwargs)
