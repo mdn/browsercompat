3c06922244afdbc166ffc41313674a2d087e3e4a
John Whitlock <jwhitlock@mozilla.com>
2016-02-25 08:57:41 -0600

    bug 1216786 - Use references in MDN importer

--- mdn/tests/base.py
+++ mdn/tests/base.py
@@ -2,7 +2,7 @@
 """Define base TestCase for MDN tests."""
 from webplatformcompat.tests.base import TestCase as BaseTestCase
 from webplatformcompat.models import (
-    Browser, Feature, Maturity, Section, Specification, Version)
+    Browser, Feature, Maturity, Reference, Section, Specification, Version)
 
 
 class TestCase(BaseTestCase):
@@ -50,6 +50,7 @@ class TestCase(BaseTestCase):
         ('Section', 'background-size'): {
             '_req': {'specification': ('Specification', 'css3_backgrounds')},
             'subpath': '{"en": "#the-background-size"}'},
+        ('Reference', ('web-css-background-size', 'background-size')): {},
         ('Feature', 'web'): {'name': '{"en": "Web"}'},
         ('Feature', 'web-css'): {
             '_req': {'parent': ('Feature', 'web')}, 'name': '{"en": "CSS"}'},
@@ -68,6 +69,16 @@ class TestCase(BaseTestCase):
         ('Version', ('firefox_desktop', '1.0')): {},
     }
 
+    name_to_class = {
+        'Browser': Browser,
+        'Feature': Feature,
+        'Maturity': Maturity,
+        'Reference': Reference,
+        'Section': Section,
+        'Specification': Specification,
+        'Version': Version,
+    }
+
     def get_instance(self, model_cls_name, slug):
         """Get a test fixture instance, creating on first access."""
         instance_key = (model_cls_name, slug)
@@ -75,6 +86,8 @@ class TestCase(BaseTestCase):
             self._instances = {}
         if instance_key not in self._instances:
             attrs = self._instance_specs[instance_key].copy()
+
+            # Load implied and explicit required instances
             req = attrs.pop('_req', {})
             if model_cls_name == 'Version':
                 browser_slug, version = slug
@@ -82,13 +95,16 @@ class TestCase(BaseTestCase):
                 attrs['version'] = version
             elif model_cls_name == 'Section':
                 attrs['name'] = '{"en": "%s"}' % slug
+            elif model_cls_name == 'Reference':
+                feature_slug, section_slug = slug
+                attrs['feature'] = self.get_instance('Feature', feature_slug)
+                attrs['section'] = self.get_instance('Section', section_slug)
             else:
                 attrs['slug'] = slug
             for req_name, (req_type, req_slug) in req.items():
                 attrs[req_name] = self.get_instance(req_type, req_slug)
-            model_cls = {
-                'Browser': Browser, 'Feature': Feature, 'Maturity': Maturity,
-                'Section': Section, 'Specification': Specification,
-                'Version': Version}[model_cls_name]
+
+            # Create the instance
+            model_cls = self.name_to_class[model_cls_name]
             self._instances[instance_key] = self.create(model_cls, **attrs)
         return self._instances[instance_key]
