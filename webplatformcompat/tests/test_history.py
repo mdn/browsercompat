10361ecba799e1f3e702c47532053ca29d8ee735
John Whitlock <jwhitlock@mozilla.com>
2016-03-02 16:09:13 -0600

    bug 1243221 - Refactor history tests
    Make history tests more like other API endpoint tests:
    * Rename base test to TestBaseMiddleware
    * Require api_data() method in override classes so that the POST
      contains valid browser creations data for that API
    * Use the NamespaceMixin more consistently
    * Use application/vnd.api+json MIME type more consistently
    * Centralize URL creation for change in the near future

--- webplatformcompat/tests/test_history.py
+++ webplatformcompat/tests/test_history.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Tests for simple_history extensions."""
 
-from json import loads
+from json import dumps, loads
 
 from django.contrib.auth.models import User
 
@@ -11,17 +11,29 @@ from webplatformcompat.models import Browser
 from .base import APITestCase
 
 
-class TestMiddleware(APITestCase):
-    """Test the HistoryChangesetRequestMiddleware."""
+class TestBaseMiddleware(APITestCase):
+    """Test the HistoryChangesetRequestMiddleware.
+
+    This should be tested by a subclass that defines:
+    * namespace - the API namespace, such as v1
+    * api_data - A function returning data in the API format
+    """
 
     __test__ = False  # Don't test outside of a versioned API
+    content_type = 'application/vnd.api+json'
+
+    def url(self, changeset):
+        """Return the test URL."""
+        return (
+            self.api_reverse('browser-list') +
+            '?changeset=%s' % changeset.id)
 
     def test_post_with_changeset(self):
         self.login_user()
         changeset = Changeset.objects.create(user=self.user)
-        url = self.api_reverse('browser-list') + '?changeset=%s' % changeset.id
-        data = {'slug': 'firefox', 'name': '{"en": "Firefox"}'}
-        response = self.client.post(url, data)
+        url = self.url(changeset)
+        response = self.client.post(
+            url, dumps(self.api_data()), content_type=self.content_type)
         self.assertEqual(201, response.status_code, response.data)
         browser = Browser.objects.get()
         history = browser.history.get()
@@ -31,10 +43,9 @@ class TestMiddleware(APITestCase):
         self.login_user()
         other = User.objects.create(username='other')
         changeset = Changeset.objects.create(user=other)
-        url = self.api_reverse('browser-list') + '?changeset=%s' % changeset.id
-        data = {'slug': 'firefox', 'name': '{"en": "Firefox"}'}
+        url = self.url(changeset)
         response = self.client.post(
-            url, data, content_type='application/vnd.api+json')
+            url, dumps(self.api_data()), content_type=self.content_type)
         self.assertEqual(400, response.status_code)
         expected = {
             'errors': {
@@ -47,10 +58,9 @@ class TestMiddleware(APITestCase):
     def test_post_with_closed_changeset(self):
         self.login_user()
         changeset = Changeset.objects.create(user=self.user, closed=True)
-        url = self.api_reverse('browser-list') + '?changeset=%s' % changeset.id
-        data = {'slug': 'firefox', 'name': '{"en": "Firefox"}'}
+        url = self.url(changeset)
         response = self.client.post(
-            url, data, content_type='application/vnd.api+json')
+            url, dumps(self.api_data()), content_type=self.content_type)
         self.assertEqual(400, response.status_code)
         expected = {
             'errors': {
@@ -60,7 +70,7 @@ class TestMiddleware(APITestCase):
     def test_post_with_error_not_json_api(self):
         self.login_user()
         changeset = Changeset.objects.create(user=self.user, closed=True)
-        url = self.api_reverse('browser-list') + '?changeset=%s' % changeset.id
+        url = self.url(changeset)
         data = {'slug': 'firefox', 'name': '{"en": "Firefox"}'}
         response = self.client.post(url, data)
         self.assertEqual(400, response.status_code)
--- webplatformcompat/tests/v2/test_history.py
+++ webplatformcompat/tests/v2/test_history.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 """Tests for simple history overrides in v2 API."""
 
-from ..test_history import TestMiddleware
+from ..test_history import TestBaseMiddleware
 from .base import NamespaceMixin
 
 
-class TestMiddlewareInV2(TestMiddleware, NamespaceMixin):
-    """Test the Middleware using API endpoints."""
+class TestMiddleware(NamespaceMixin, TestBaseMiddleware):
+    """Test the Middleware using v2 API endpoints."""
+
+    def api_data(self):
+        """Return JSON data for creating a new browser."""
+        return {
+            'data': {
+                'type': 'browsers',
+                'attributes': {
+                    'slug': 'firefox',
+                    'name': {
+                        'en': 'Firefox'
+                    }
+                }
+            }
+        }
